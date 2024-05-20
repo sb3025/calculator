b50ac3c5f5b6cdf978bad7b2de84a63aecb54656
developer1 <dev1@email.com>
2024-05-20 09:33:25 +0900

    Modify ui.py to add le1, le2 and cb widgets

--- ui.py
+++ ui.py
@@ -1,7 +1,8 @@
-# ch 5.2.1 ui.py
-# 기존 main.py의 calculator 클래스에서 윈도 화면 정의, 초기화, 그리고 텍스트 에디트 화면 처리 부분만 분리
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout)
+# ch 6.2.1 ui.py
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout, QLineEdit, QComboBox) # QLineEdit, QComboBox 추가
 from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore # 모듈 추가
+
 
 class View(QWidget):
     
@@ -20,9 +21,24 @@ class View(QWidget):
         hbox.addStretch(1)
         hbox.addWidget(self.btn1)
         hbox.addWidget(self.btn2)
+
+        self.le1 = QLineEdit('0', self) # 라인 에디트 1 추가
+        self.le1.setAlignment(QtCore.Qt.AlignRight) # 라인 에디트 1 문자열 배치 설정
+
+        self.le2 = QLineEdit('0', self) # 라인 에디트 2 추가
+        self.le2.setAlignment(QtCore.Qt.AlignRight) # 라인 에디트 2 문자열 배치 설정
+
+        self.cb = QComboBox(self) # 콤보 박스 추가
+        self.cb.addItems(['+', '-', '*', '/']) # 콤보 박스 항목 추가(연산자로 사용)
+
+        hbox_formular = QHBoxLayout() # 새로 정의한 위젯을 QHBoxLayout에 배치
+        hbox_formular.addWidget(self.le1)
+        hbox_formular.addWidget(self.cb)
+        hbox_formular.addWidget(self.le2)
         
         vbox=QVBoxLayout()
         vbox.addWidget(self.te1)
+        vbox.addLayout(hbox_formular) # hbox_formular 배치
         vbox.addLayout(hbox)
         vbox.addStretch(1)
         
