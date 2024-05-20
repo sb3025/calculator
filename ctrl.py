9e2c333eb36bbad1664458d364f959e92f2c3cd7
developer2 <dev2@email.com>
2024-05-20 09:55:52 +0900

    Add sum function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -1,4 +1,4 @@
-# ch 5.2.1 ctrl.py
+# ch 6.4.1 ctrl.py
 # ui에서 입력되는 이벤트 처리나 ui 동작 제어와 관련된 내용 포함
 class Control:
     
@@ -12,3 +12,6 @@ class Control:
     def connectSignals(self):
         self.view.btn1.clicked.connect(self.calculate) # 버튼 1 연결(ui.py에 정의된 버튼 위젯 btn1을 클릭하면 계산하도록 구현)을 변경
         self.view.btn2.clicked.connect(self.view.clearMessage)
+
+    def sum(self, a, b): # 덧셈 함수 추가
+        return a+b
